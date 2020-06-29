e98fdb1d99ee10f15f42934a93772bd954769bdd
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-06-29 06:59:04 +0530

    completed about page

--- src/components/about.js
+++ src/components/about.js
@@ -3,8 +3,18 @@ import React, { Component } from "react";
 class About extends Component {
   render() {
     return (
-      <div id="about">
-        <h1>About page</h1>
+      <div className="about">
+        <img src="/images/ts2.jpg" alt="" />
+        <h2>Hi, I'm firstname lastname.</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
       </div>
     );
   }
